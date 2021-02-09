b6f6176dd5c5f8b91125f01c4bb1a7ffae37796a
Dang <huynhminhdang@gmail.com>
2021-02-10 06:12:17 +0800

    only fix PATH when installing package globally

--- functions/pnpm.fish
+++ functions/pnpm.fish
@@ -1,7 +1,7 @@
 function pnpm
   set --local args $argv
   argparse --ignore-unknown 'g/global' -- $argv
-  if set --query _flag_global
+  if set --query _flag_global; and contains argv[1] i install add
     # prepend PATH with global-dir so global binaries will be linked correctly
     PATH=(command npm get global-dir)/bin:$PATH command pnpm $args
   else
